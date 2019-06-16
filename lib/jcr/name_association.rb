5d9993bfbb4d8ca04e7b70f2d80853883d2232fe
Pete Cordell <git@codalogic.com>
2019-06-16 15:56:28 +0100

    Add NameAssociation.to_key() method

---
+++ lib/jcr/name_association.rb
@@ -0,0 +1,44 @@
+# Copyright (c) 2015-2019 American Registry for Internet Numbers
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+# IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+require 'jcr/parser'
+require 'jcr/map_rule_names'
+require 'jcr/check_groups'
+require 'jcr/evaluate_rules'
+
+module JCR
+
+  class NameAssociation
+    def initialize
+    end
+    
+    def self.to_key rule
+      if rule[:member_name]
+        return "l" + rule[:member_name][:q_string].to_s
+      elsif rule[:member_regex][:regex]
+        if rule[:member_regex][:regex].is_a? Array
+          return "w"    # Wildcard = //
+        else
+          re_modifiers =
+                  rule[:member_regex][:regex_modifiers].is_a?( Array ) ?
+                  "" :
+                  rule[:member_regex][:regex_modifiers].to_s
+          return "r" + re_modifiers + "/" + rule[:member_regex][:regex]
+        end
+      end
+      #else
+      raise JCR::JcrValidatorError, "Unrecognised member name format for rule: " + rule.to_s
+    end
+  end
+end
---
+++ spec/name_association_spec.rb
@@ -0,0 +1,41 @@
+# Copyright (C) 2015-2016 American Registry for Internet Numbers (ARIN)
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+# IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+require 'spec_helper'
+require 'rspec'
+require 'pp'
+require_relative '../lib/jcr/name_association'
+
+describe 'name_association' do
+
+  it 'should give a key of "lmn" for literal member name "mn"' do
+    tree = JCR.parse( '$rrule = "mn" :integer' )
+    expect( JCR::NameAssociation.to_key( tree[0][:rule][:member_rule] ) ).to eq( "lmn" )
+  end
+
+  it 'should give a key of "r/mn" for regex member name /mn/' do
+    tree = JCR.parse( '$rrule = /mn/ :integer' )
+    expect( JCR::NameAssociation.to_key( tree[0][:rule][:member_rule] ) ).to eq( "r/mn" )
+  end
+
+  it 'should give a key of "rix/mn" for regex member name /mn/ix' do
+    tree = JCR.parse( '$rrule = /mn/ix :integer' )
+    expect( JCR::NameAssociation.to_key( tree[0][:rule][:member_rule] ) ).to eq( "rix/mn" )
+  end
+
+  it 'should give a key of "w" for wildcard member name //' do
+    tree = JCR.parse( '$rrule = // :integer' )
+    expect( JCR::NameAssociation.to_key( tree[0][:rule][:member_rule] ) ).to eq( "w" )
+  end
+
+end
